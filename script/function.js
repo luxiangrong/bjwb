e1050f34669908bd69a348ac1d42276cfab42769
luxiangrong <luxiangrongwave@gmail.com>
2014-05-26 17:03:13 +0800

    首页新闻其他完成一半

--- script/function.js
+++ script/function.js
@@ -1,6 +1,7 @@
 jQuery.noConflict();
 (function($) {
 	$(function() {
+		//焦点新闻
 		$(".hot-news .col-list li").on('mouseover', function(){
 			var newsWidth = 620;
 			var index = $(this).index();
@@ -15,6 +16,58 @@ jQuery.noConflict();
 			    queue: false,
 			    easing: 'easeInQuint'
 			});
+		});
+		
+		//杂志
+		$(".row-magazine .content-body").find("ul").width($(".row-magazine .content-body").find("ul li").size() * $(".row-magazine .content-body").find("ul li").outerWidth(true));
+		$(".row-magazine .content-body").mCustomScrollbar({
+			 axis:"x"
+		});
+		
+		//
+		var extraNews = {
+			prev: function($parent){
+				var current = $parent.data('index') == undefined ? 0 : parseInt($parent.data('index'));
+				if(current <= 0) {
+					current = $parent.find('li').length - 1
+				} else {
+					current = current - 1;
+				}
+				var itemWidth = $parent.find('li').outerWidth(true);
+				$parent.animate({
+					left: itemWidth * current * -1
+				},{
+				    duration: 500,
+				    queue: false,
+				    easing: 'easeInQuint'
+				});
+				$parent.data('index', current);
+			},
+			next: function($parent){
+				var current = $parent.data('index') == undefined ? 0 : parseInt($parent.data('index'));
+				
+				if(current >= $parent.find('li').length - 1) {
+					current = 0;
+				} else {
+					current = current + 1;
+				}
+				
+				var itemWidth = $parent.find('li').outerWidth(true);
+				$parent.animate({
+					left: itemWidth * current * -1
+				},{
+				    duration: 500,
+				    queue: false,
+				    easing: 'easeInQuint'
+				});
+				$parent.data('index', current);
+			}
+		}
+		$(".news-extra-item a.btn-left").click(function(){
+			extraNews.prev($(this).parent().find('ul'));
+		});
+		$(".news-extra-item a.btn-right").click(function(){
+			extraNews.next($(this).parent().find('ul'));
 		});
 	});
 })(jQuery);
